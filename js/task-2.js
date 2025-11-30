c4e8cd610291e3df0dc5d94ecee3520608597087
Artem <stuzukartem12@gmail.com>
2025-11-30 16:25:06 +0200

    js

--- js/task-2.js
+++ js/task-2.js
@@ -1,9 +1,9 @@
 function formatMessage(message, maxLength){
     // const muy = message.slice()
-    if(message<=maxLength){
-        return message;
+      if (message.length <= maxLength) {
+        returnп message;
     }else{
-        return `"${message.slice(0,maxLength)}..."`;
+        return `${message.slice(0,maxLength)}...`;
     }
 }
 console.log(formatMessage("Curabitur ligula sapien", 16)); // "Curabitur ligula..."
